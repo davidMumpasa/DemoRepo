fa403d855b20d6dbccbd0fd7bb1b639e9ac99a88
hp <davidebula642@gmail.com>
2022-04-27 14:47:57 +0200

    first commit

---
+++ src/demoapplication/DemoApplication.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package demoapplication;
+
+/**
+ *
+ * @author hp
+ */
+public class DemoApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+       int num1=5,num2=29,sum=0;
+
+       sum = num1+num2;
+        System.out.println("the sum is:"+sum);
+    }
+    
+}
